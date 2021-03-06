d3178be3293ac6b365c47f0f7546119479fc1656
Lucas Dicésaro <lucasdicesaro@gmail.com>
2021-03-06 11:18:51 -0300

    Adding mongoDB connection. Adding a README sketch

---
+++ README.md
@@ -0,0 +1,25 @@
+# COVID STATS SERVICE
+
+This API provides utils stats about COVID.
+
+## Prerequisites
+
+- MongoDB
+
+
+## Used Node libraries
+- express
+- mongoose
+- dotenv (just for dev)
+- nodemon (just for dev)
+
+
+## Dev notes
+A .env file must be created in root folder with database connection.
+```
+DATABASE_HOST=mongodb://localhost/covid_stats_db
+```
+Startup the server
+```
+npm run devStart
+```
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', (error) => { console.error(error) })
+db.once('open', () => { console.log('Database connected') })
+
+app.use(express.json())
 
 const covidRouter = require('./routes/covid')
 app.use('/covid', covidRouter)
